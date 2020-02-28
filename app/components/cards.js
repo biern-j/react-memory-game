4743567ec0ea2e3e17273164075b4c30b23cad10
anzamarska <anzamarska@gmail.com>
2020-02-28 23:15:10 +0100

    adding color changes

--- app/components/cards.js
+++ app/components/cards.js
@@ -3,7 +3,7 @@ import React from "react";
 export function Cards({number, cards}) {
     const makeButtons = (card) => {
         console.log(card);
-    return <button key={card.id} >{card.id}{card.color}</button>;
+    return <button key={card.id} style={{backgroundColor: card.color}}>{card.id}{card.color}</button>;
     };
     const map1 = cards.map(item => makeButtons(item));
 
