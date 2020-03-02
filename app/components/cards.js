5efe8ceb4cb66918353683cd32c63fba55ed264c
Jola Wroniecka <wronieckajolanta@gmail.com>
2020-03-02 11:43:17 +0100

    Set toggle method

--- app/components/cards.js
+++ app/components/cards.js
@@ -1,11 +1,20 @@
 import React from "react";
 
-export function Cards({number, cards}) {
-    const makeButtons = (card) => {
-        console.log(card);
-    return <button key={card.id} style={{backgroundColor: card.color}}>{card.id}{card.color}</button>;
-    };
-    const map1 = cards.map(item => makeButtons(item));
+export function Cards({ number, cards, onClick }) {
+  const makeButtons = card => {
+    console.log(card);
+    return (
+      <button
+        onClick={() => onClick(card.id)}
+        key={card.id}
+        style={{ backgroundColor: card.color }}
+      >
+        {card.id}
+        {card.color}
+      </button>
+    );
+  };
+  const map1 = cards.map(item => makeButtons(item));
 
-    return map1
+  return map1;
 }
