34dd455abe5508dc3b41afa9f9d6a314d3ddb113
Jola Wroniecka <wronieckajolanta@gmail.com>
2020-03-02 12:44:53 +0100

    Set back card gray color

--- app/components/cards.js
+++ app/components/cards.js
@@ -2,12 +2,13 @@ import React from "react";
 
 export function Cards({ cards, getIdOnClick }) {
   const makeButtons = card => {
+    console.log("card.disabled", card.disable);
     return (
       <button
-        disabled={card.disabled}
+        disabled={card.disable}
         onClick={() => getIdOnClick(card.id)}
         key={card.id}
-        style={{ backgroundColor: card.color }}
+        style={{ backgroundColor: card.disable ? card.color : "gray" }}
       >
         {card.id}
         {card.color}
