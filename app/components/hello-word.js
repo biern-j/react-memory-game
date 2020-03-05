80093469b41289685ef2d4d76b729da141a6f33f
Jola Wroniecka <wronieckajolanta@gmail.com>
2020-03-05 08:36:56 +0100

    Set memory game basic functionality

--- app/components/hello-word.js
+++ app/components/hello-word.js
@@ -1,9 +1,10 @@
 import React from "react";
 
-export function HelloWord({color, names}) {
-  console.log("Hello world props", color, names);
-  return <div>
-    <div>Hello World {names[0]}</div>
-    <div>Hello World {names[1]}</div>
-    </div>;
+export function HelloWord({ names }) {
+  return (
+    <div>
+      <div>Hello World {names[0]}</div>
+      <div>Hello World {names[1]}</div>
+    </div>
+  );
 }
--- app/index.js
+++ app/index.js
@@ -9,70 +9,69 @@ class App extends React.Component {
     this.state = {
       names: ["Jola", "Ania"],
       cards: [
-        { id: 0, color: "red", disable: false, find: false },
-        { id: 1, color: "red", disable: false, find: false },
-        { id: 2, color: "blue", disable: false, find: false },
-        { id: 3, color: "blue", disable: false, find: false }
+        { id: 0, color: "red", clicked: false, found: false },
+        { id: 1, color: "red", clicked: false, found: false },
+        { id: 2, color: "blue", clicked: false, found: false },
+        { id: 3, color: "blue", clicked: false, found: false }
       ]
     };
   }
+  scheduleHideCard() {
+    setTimeout(
+      () =>
+        this.setState(state => ({
+          cards: this.undisableClickedCard(state)
+        })),
+      1000
+    );
+  }
+
+  undisableClickedCard(state) {
+    return state.cards.map(card => ({
+      ...card,
+      clicked: false
+    }));
+  }
 
   setToggle(id) {
-    // this.state.cards[id].disable = !this.state.cards[id].disable;
-    // this.setState({ cards: this.state.cards });
+    const toggleClickedCard = state =>
+      state.cards.map(card =>
+        card.id === id ? { ...card, clicked: !card.clicked } : card
+      );
+    const setFoundCard = (cards, disabledCards) =>
+      cards.map(card =>
+        card.id === disabledCards[0].id || card.id === disabledCards[1].id
+          ? { ...card, clicked: card.clicked, found: true }
+          : card
+      );
 
-    let toggledCards = [];
+    this.setState(state => {
+      const toggleClickedCards = toggleClickedCard(state);
+      const filterDisabledCards = toggleClickedCards.filter(
+        card => card.clicked
+      );
 
-    for (let i = 0; i < this.state.cards.length; i++) {
-      if (this.state.cards[i].id === id) {
-        toggledCards.push(this.state.cards[i]);
-        if (toggledCards.length === 2) {
-          this.setState({
-            cards: this.state.cards.map(card =>
-              toggledCards.map(toggledCard => {
-                if (
-                  card.id === toggledCard.id &&
-                  toggledCards[0].color === toggledCards[1].color
-                ) {
-                  console.log("find", card.find);
-                  return { ...card, find: !card.find };
-                }
-                return card;
-              })
-            )
-          });
-          // toggledCards.map(x => {
-          //   if (toggledCards[0].color === toggledCards[1].color) {
-          //     // this.state.cards[x.id].find = !this.state.cards[x.id].find;
-          //     this.setState({
-          //       cards: this.state.cards.map(card =>
-          //         toggledCards.map(toggledCard => {
-          //           if (card.id === toggledCard.id && toggledCards[0].color === toggledCards[1].color) {
-          //             console.log("find", card.find);
-          //             return { ...card, find: !card.find };
-          //           }
-          //           return card;
-          //         })
-          //       )
-          //     });
-          //   }
-          //   // this.state.cards[x.id].disable = !this.state.cards[x.id].disable;
-          //   // this.setState({ cards: this.state.cards });
-          // });
+      if (filterDisabledCards.length === 2) {
+        if (filterDisabledCards[0].color === filterDisabledCards[1].color) {
+          return {
+            cards: setFoundCard(toggleClickedCards, filterDisabledCards)
+          };
+        } else {
+          this.scheduleHideCard();
         }
       }
-    }
+
+      return {
+        cards: toggleClickedCards
+      };
+    });
   }
 
   render() {
-    console.log("names", this.state.cards);
     return (
       <div>
-        <HelloWord color={"blue"} names={this.state.names} />
-        <Cards
-          cards={this.state.cards}
-          getIdOnClick={id => this.setToggle(id)}
-        />
+        <HelloWord names={this.state.names} />
+        <Cards cards={this.state.cards} onClick={id => this.setToggle(id)} />
       </div>
     );
   }
