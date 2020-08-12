4b890c0199b8479ef3085ccd4b5e3a0e243ec604
jolawro <wronieckajolanta@gmail.com>
2020-08-12 12:26:15 +0200

    Set cypress test; add first test

---
+++ cypress/integration/gameForm/gameFormTest.ts
@@ -0,0 +1,10 @@
+context('Actions', () => {
+    beforeEach(() => {
+      cy.visit('http://localhost:8080/')
+    })
+    it('Add player', () => {
+        cy.get('[data-cy=new-player-input]').type("Jola");
+        cy.get('[data-cy=submit-player]').click();
+        cy.get('[data-cy=start-game]').click();
+    })
+})
