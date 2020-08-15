f90d772305a6f2ae20411d1844e4fbec6264ae4a
jolawro <wronieckajolanta@gmail.com>
2020-08-15 11:09:17 +0200

    Refactoring levelButton component

--- app/components/levelButton.tsx
+++ app/components/levelButton.tsx
@@ -2,24 +2,29 @@ import * as React from "react";
 import { LevelInput, LevelBox, LevelForm } from "./levelButtonStyle";
 type LevelInputProps = {
   levelButtons: GameDifficultyLevel[];
-  onClick: (value: string, e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+  onClick: (
+    value: string,
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => void;
 };
 
 export type GameDifficultyLevel = {
   levelTitle: string;
   multiple: number;
-  choosen: boolean;
+  chosen: boolean;
 };
 
-export const LevelButtons = ({ levelButtons, onClick }: LevelInputProps) =>
-<LevelForm>{levelButtons.map(item =>
+export const LevelButtons = ({ levelButtons, onClick }: LevelInputProps) => (
+  <LevelForm>
+    {levelButtons.map((item) => (
       <LevelBox key={item.levelTitle}>
-     
-     <LevelInput selectedDifficulty={item.choosen}
-      onClick={(e) => onClick(item.levelTitle, e)}
-     >
-     {item.levelTitle}
-     </LevelInput>
-     </LevelBox>)}
-     </LevelForm>;
-
+        <LevelInput
+          selectedDifficulty={item.chosen}
+          onClick={(e) => onClick(item.levelTitle, e)}
+        >
+          {item.levelTitle}
+        </LevelInput>
+      </LevelBox>
+    ))}
+  </LevelForm>
+);
--- app/components/levelButtonStyle.tsx
+++ app/components/levelButtonStyle.tsx
@@ -1,42 +1,45 @@
 import styled from "styled-components";
-import {device} from '../RWD';
+import { device } from "../RWD";
 
 export const LevelForm = styled.div`
-border-radius: 5px;
-width: 100%;
+  border-radius: 5px;
+  width: 100%;
 
-@media screen ${device.mobileS} {
+  @media screen ${device.mobileS} {
     width: 60%;
-}
+  }
 `;
 
-export const LevelInput = styled.button<{selectedDifficulty: boolean}>`
-display: inline-block;
-    border: none;
-    border-radius: 50px;
-    margin-top: 2rem;
-    padding: 1rem 3rem;
-    cursor: pointer;
-    text-transform: uppercase;
-    color: #FEE4C4;
-    font-weight: bold;
-    transition: al1s;
- ${({selectedDifficulty}) => selectedDifficulty ? "background-color: #7C79A2" : "background-color: #F57D7C"};
-&:hover {
-    background-color: #7C79A2;
-    color: #F57D7C;
+export const LevelInput = styled.button<{ selectedDifficulty: boolean }>`
+  display: inline-block;
+  border: none;
+  border-radius: 50px;
+  margin-top: 2rem;
+  padding: 1rem 3rem;
+  cursor: pointer;
+  text-transform: uppercase;
+  color: #fee4c4;
+  font-weight: bold;
+  transition: al1s;
+  ${({ selectedDifficulty }) =>
+    selectedDifficulty
+      ? "background-color: #7C79A2"
+      : "background-color: #F57D7C"};
+  &:hover {
+    background-color: #7c79a2;
+    color: #f57d7c;
     transform: scale(1.2);
-}
+  }
 
-@media screen ${device.mobileS} {
+  @media screen ${device.mobileS} {
     display: inline;
     padding: 1rem 1.5rem;
-    margin: .5rem;
-}
-`
+    margin: 0.5rem;
+  }
+`;
 
 export const LevelBox = styled.div`
-display: inline-block;
-padding: 1rem 3rem;
-margin-top: 2rem;
+  display: inline-block;
+  padding: 1rem 3rem;
+  margin-top: 2rem;
 `;
